7f4af523bacfcc3efb2e799a7216498d7369de83
Frenzy277 <tomas.tomecek@hotmail.com>
2014-11-12 10:19:11 +0100

    add login, logout, registration, sessions_controller, default views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_secure_password, validations: false
+  has_secure_password validations: false
 
   has_many :user_projects
   has_many :projects, through: :user_projects
